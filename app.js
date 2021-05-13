35f96a1c8f20f873cf737f14cb4add2612289b4b
Matt <msm3390@gmail.com>
2021-05-12 22:24:43 -0400

    pushit

--- app.js
+++ app.js
@@ -68,14 +68,14 @@ this.forEach(element => {
       }, { sum: 0, count: 0 });
     ////////////////////////////////////////////////////////
     const squareList = arr => {
-        return arr.reduce((sqrIntegers, num) => {
-          return Number.isInteger(num) && num > 0
-            ? sqrIntegers.concat(num * num)
-            : sqrIntegers;
-        }, []);
-      };
-      
-      
-      
-      
+  return arr.reduce((sqrIntegers, num) => {
+    return Number.isInteger(num) && num > 0
+      ? sqrIntegers.concat(num * num)
+      : sqrIntegers;
+  }, []);
+};
+
+
+
+
   
